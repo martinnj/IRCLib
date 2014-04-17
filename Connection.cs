a220c632f5e789b674187a6aba865af67b4531f2
Martin J <martinnjorgensen@gmail.com>
2014-04-18 00:15:58 +0200

    herp

--- Connection.cs
+++ Connection.cs
@@ -6,7 +6,14 @@ using System.Threading.Tasks;
 
 namespace IRCLib
 {
+    /// <summary>
+    /// Class representing a connection to an IRC server.
+    /// </summary>
     class Connection
     {
+        public Connection(string hostname, int port, string userMessage, string nick)
+        {
+            
+        }
     }
 }
--- IRCLib.cs
+++ IRCLib.cs
@@ -4,8 +4,39 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace IRCLib
 {
+    /// <summary>
+    /// Enum type for the possibe replies an IRC server might send back.
+    /// </summary>
+    public enum Replies
+    {
+        // ReSharper disable InconsistentNaming
+
+        // Registration replies.
+        RPL_WELCOME          = 001,
+        RPL_YOURHOST         = 002,
+        RPL_CREATED          = 003,
+        RPL_MYINFO           = 004,
+        RPL_BOUNCE           = 005,
+
+        // Error replies.
+        ERR_NOSUCHNICK       = 401,
+        ERR_NOSUCHSERVER     = 402,
+        ERR_NOSUCHCHANNEL    = 403,
+        ERR_CANNOTSENDTOCHAN = 404,
+        ERR_TOOMANYCHANNELS  = 405,
+        ERR_WASNOSUCHNICK    = 406,
+        ERR_TOOMANYTARGETS   = 407,
+        ERR_NOSUCHSERVICE    = 408,
+        ERR_NOORIGIN = 409
+        // ReSharper restore InconsistentNaming
+    };
+
+    /// <summary>
+    /// Baseclass for the entire library. So far, might change. you know. :)
+    /// </summary>
     public class IRCLib
     {
     }
