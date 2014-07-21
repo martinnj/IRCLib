6cf88ced4c4aa84a0f5dc6d2cc361ddf6d02a9e5
Martin J <martinnjorgensen@gmail.com>
2014-07-21 16:03:42 +0200

    Planning stuff.

--- IRCClient/Form1.Designer.cs
+++ IRCClient/Form1.Designer.cs
@@ -28,20 +28,109 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.mainsplitter = new System.Windows.Forms.SplitContainer();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.minimizeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.roomlist = new System.Windows.Forms.TreeView();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.mainsplitter)).BeginInit();
+            this.mainsplitter.Panel1.SuspendLayout();
+            this.mainsplitter.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Location = new System.Drawing.Point(0, 469);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(880, 22);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(880, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // mainsplitter
+            // 
+            this.mainsplitter.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mainsplitter.Location = new System.Drawing.Point(0, 24);
+            this.mainsplitter.Name = "mainsplitter";
+            // 
+            // mainsplitter.Panel1
+            // 
+            this.mainsplitter.Panel1.Controls.Add(this.roomlist);
+            this.mainsplitter.Size = new System.Drawing.Size(880, 445);
+            this.mainsplitter.SplitterDistance = 129;
+            this.mainsplitter.TabIndex = 2;
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.minimizeToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // minimizeToolStripMenuItem
+            // 
+            this.minimizeToolStripMenuItem.Name = "minimizeToolStripMenuItem";
+            this.minimizeToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.minimizeToolStripMenuItem.Text = "Minimize";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // roomlist
+            // 
+            this.roomlist.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.roomlist.Location = new System.Drawing.Point(0, 0);
+            this.roomlist.Name = "roomlist";
+            this.roomlist.Size = new System.Drawing.Size(129, 445);
+            this.roomlist.TabIndex = 0;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(880, 491);
+            this.Controls.Add(this.mainsplitter);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.mainsplitter.Panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.mainsplitter)).EndInit();
+            this.mainsplitter.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem minimizeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.SplitContainer mainsplitter;
+        private System.Windows.Forms.TreeView roomlist;
     }
 }
 
